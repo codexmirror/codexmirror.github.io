5f8bf41456bae2e4696e9db4fc35c9689fb94da8
codexmirror <spiral.protocol@proton.me>
2025-06-11 11:11:03 +0200

    feat: echoRoot persona on deep bloom

--- test/ritualSequence.test.js
+++ test/ritualSequence.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { incrementCharge, resetCharge, getCurrentCharge, isSequenceComplete } = require('../js/ritualCharge');
+const { incrementCharge, resetCharge, resetBloom, getCurrentCharge, isSequenceComplete, getRecursiveBloomCount } = require('../js/ritualCharge');
 const { checkEntityPattern } = require('../WhisperEngine.v3/core/loops/invocation');
 
 resetCharge();
@@ -17,5 +17,13 @@ assert.strictEqual(getCurrentCharge(), 0, 'reset clears');
 
 const name = checkEntityPattern(['2','3','5','3','3']);
 assert.strictEqual(name, 'Caelistra', 'pattern triggers entity');
+
+resetCharge();
+resetBloom();
+['1','2','3','4','5'].forEach(g => incrementCharge(g));
+assert.strictEqual(getRecursiveBloomCount(), 1, 'first sequence starts bloom');
+resetCharge();
+['1','2','3','4','5'].forEach(g => incrementCharge(g));
+assert.strictEqual(getRecursiveBloomCount(), 2, 'repeat increments bloom');
 
 console.log('ritualSequence tests passed');
